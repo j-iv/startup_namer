717692a34ace9b1ffeb86b4599c2586eb4bb827e
JishnuV <jishnu.v@target.com>
2022-10-16 13:36:03 +0530

    added a stateful widget for infinite scrolling

--- lib/main.dart
+++ lib/main.dart
@@ -10,17 +10,46 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter 2.0',
+      title: 'Startup name generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter 2.0'),
+          title: const Text('Startup name generator 1.0'),
         ),
-        body: Center(
-          child: Text(wordPair.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: (context, i) {
+      if (i.isOdd) {
+        return const Divider();
+      }
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+      return ListTile(
+        title: Text(
+          _suggestions[index].asPascalCase,
+          style: _biggerFont,
+        ),
+      );
+    });
   }
 }
